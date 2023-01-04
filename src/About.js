df8d5401166c51c98cbf91ca4d13008b3e72a90f
Kellen Anker <kellenanker7@gmail.com>
2023-01-03 18:41:14 -0700

    avout updates

--- src/About.js
+++ src/About.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import { colors } from "./Constants.js";
 import Header from "./Header";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const About = () => {
   return (
@@ -10,64 +10,54 @@ const About = () => {
       <Header active="About" />
       <Container>
         <Row>
-          <h2>Leaderboard</h2>
-          <p>
-            The <Link to="/leaderboard">leaderboard</Link> ranks users by their
-            average number of guesses taken to solve Wordles. Users must solve
-            at least three Wordles over the given time period to earn a place on
-            the leaderboard.
-          </p>
-          <p>
-            Users achieve a "hot streak" (🔥) after successfuly completing three
-            consecutive Wordles. Skipping a Wordle snaps a streak, as does
-            failing to solve a Wordle.
-          </p>
+          <Col>
+            <h2>Leaderboard</h2>
+            <p>
+              The <Link to="/leaderboard">leaderboard</Link> ranks users by
+              their average number of guesses taken to solve Wordles. Users must
+              solve at least three Wordles over the given time period to earn a
+              place on the leaderboard.
+            </p>
+            <p>
+              Users achieve a "hot streak" (🔥) after successfuly completing
+              three consecutive Wordles. Skipping a Wordle snaps a streak, as
+              does failing to solve a Wordle.
+            </p>
+          </Col>
         </Row>
         <Row>
-          <h2>Acknowledgements</h2>
-          <p>
-            Wordle definitions powered by{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://dictionaryapi.dev/"
-            >
-              Free Dictionary API
-            </a>
-            . Wordle archive powered by{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://wordfinder.yourdictionary.com/wordle/answers/"
-            >
-              WordFinder
-            </a>
-            . Timezone data powered by{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://worldtimeapi.org"
-            >
-              WorldTimeAPI
-            </a>
-            .
-          </p>
-        </Row>
-        <Row>
-          <h2>Color scheme</h2>
-          <p>
-            The following color scheme is used throughout this app to denote
-            users' proficiency at solving Wordles:
-          </p>
-          <div>
-            {colors.map((e, i) => (
-              <div key={i} style={{ background: e, textAlign: "center" }}>
-                {i <= 1 && "Solved in 1 guess"}
-                {i > 1 && i < 7 && `Solved in ${i} guesses`}
-                {i >= 7 && "Failed to solve"}
-              </div>
-            ))}
-          </div>
+          <Col>
+            <h2>Acknowledgements</h2>
+            <ul>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://dictionaryapi.dev/"
+                >
+                  Free Dictionary API
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://wordfinder.yourdictionary.com/wordle/answers/"
+                >
+                  WordFinder
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://worldtimeapi.org"
+                >
+                  WorldTimeAPI
+                </a>
+              </li>
+            </ul>
+          </Col>
         </Row>
       </Container>
     </>
